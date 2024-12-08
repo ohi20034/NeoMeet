54a8ea0df8b829db3ad6677d28ddbf886e9c88c6
Imdadul Haque Ohi <imdadul0202@gmail.com>
2024-12-08 16:10:32 +0600

    update

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
+const pool = require('./util/database');
+
+app.get('/', (req, res) => {
     res.send("hello world");
 })
 
-app.listen(3000);
 
+async function checkDbConnection() {
+    console.log('hi');
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error connecting to the database:', err.message);
+        } else {
+            console.log('Database is connected!');
+            app.listen(process.env.PORT || 3000, () => {
+                console.log(`Server running on port ${process.env.PORT || 3000}`);
+            });
+
+        }
+    });
+}
+
+checkDbConnection();
